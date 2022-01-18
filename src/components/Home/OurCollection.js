89d6efe1df90c588a4a43fb925e982218af7eca4
David Kapteyn <dave.kapteyn@gmail.com>
2022-01-17 20:53:23 -0700

    created OurCollectionComponent

--- src/components/Home/OurCollection.js
+++ src/components/Home/OurCollection.js
@@ -1,32 +1,57 @@
 export default function OurCollection() {
-  
-  
-  
-  
-  
-  
-  
-    return (
+  return (
     <div className="home--collectionContainer">
+      <div className="collectionTitleContainer">
+      <h1 className="collectionTitle">our collection</h1> 
+      </div> 
+      <div className="coffeeTypeContainer">
       <div className="coffeeType">
-        <img className="desktopBeanBagPic" src="/mainAssets/assets/home/desktop/image-gran-espresso.png" alt="Coffee bean bag" />
+        <img
+          className="desktopBeanBagPic"
+          src="/mainAssets/assets/home/desktop/image-gran-espresso.png"
+          alt="Coffee bean bag"
+        />
         <h4 className="beanName"> Gran Espresso</h4>
-        <p className="beanDescription">Light and flavorful blend with cocoa and black pepper for an intense experience</p>
+        <p className="beanDescription">
+          Light and flavorful blend with cocoa and black pepper for an intense
+          experience
+        </p>
       </div>
       <div className="coffeeType">
-        <img className="desktopBeanBagPic" src="/mainAssets/assets/home/desktop/image-danche.png" alt="Coffee bean bag" />
+        <img
+          className="desktopBeanBagPic"
+          src="/mainAssets/assets/home/desktop/image-planalto.png"
+          alt="Coffee bean bag"
+        />
         <h4 className="beanName"> Planalto</h4>
-        <p className="beanDescription">Brazilian dark roast with rich and velvety body, and hints of fruits and nuts</p>
+        <p className="beanDescription">
+          Brazilian dark roast with rich and velvety body, and hints of fruits
+          and nuts
+        </p>
       </div>
       <div className="coffeeType">
-        <img className="desktopBeanBagPic" src="/mainAssets/assets/home/desktop/image-piccollo.png" alt="Coffee bean bag" />
+        <img
+          className="desktopBeanBagPic"
+          src="/mainAssets/assets/home/desktop/image-piccollo.png"
+          alt="Coffee bean bag"
+        />
         <h4 className="beanName">Piccollo</h4>
-        <p className="beanDescription"> Mild and smooth blend featuring notes of toasted almond and dried cherry</p>
+        <p className="beanDescription">
+          Mild and smooth blend featuring notes of toasted almond and dried
+          cherry
+        </p>
       </div>
       <div className="coffeeType">
-        <img className="desktopBeanBagPic" src="/mainAssets/assets/home/desktop/image-planalto.png" alt="Coffee bean bag" />
+        <img
+          className="desktopBeanBagPic"
+          src="/mainAssets/assets/home/desktop/image-danche.png"
+          alt="Coffee bean bag"
+        />
         <h4 className="beanName">Danche</h4>
-        <p className="beanDescription">Ethiopian hand-harvested blend densely packed with vibrant fruit notes</p>
+        <p className="beanDescription">
+          Ethiopian hand-harvested blend densely packed with vibrant fruit notes
+        </p>
+      </div>
       </div>
     </div>
   );
