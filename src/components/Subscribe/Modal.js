47276974bf04a99bcae8fa8588ba7761592d76ce
DKapteyn <86522409+DKapteyn@users.noreply.github.com>
2022-02-05 08:34:35 -0700

    styled modal

--- src/components/Subscribe/Modal.js
+++ src/components/Subscribe/Modal.js
@@ -5,15 +5,19 @@ export default function Modal({ openModal, toggle }) {
   return (
     <div onClick={toggle} className="Modal--Overlay">
       <div className="Modal--Container">
-        <div className="Modal--title">Order Summary</div>
-        <OrderSummaryTemplate classPrefix={"modal"} />
-        <p className="Modal--isCorrect">
+        <div className="Modal--titleContainer">
+          <div className="Modal--title">Order Summary</div>
+        </div>
+        <OrderSummaryTemplate classPrefix={"Modal"} />
+        <p className="Modal--para">
           Is this correct? You can proceed to checkout or go back to plan
           selection if something is off. Subscription discount codes can also be
           redeemed at the checkout.
         </p>
-        <div className="Modal--monthTotal">14.00/mo</div>
-        <button className="button">Checkout</button>
+        <div className="Modal--totalAndButtonContainer">
+          <div className="Modal--monthTotal">$14.00/mo</div>
+          <button className="Button">Checkout</button>
+        </div>
       </div>
     </div>
   );
