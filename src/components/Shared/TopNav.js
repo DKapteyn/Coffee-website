67cf5a4f0db74ffd7f80e6751154669dabb468c7
David Kapteyn <dave.kapteyn@gmail.com>
2022-03-07 06:15:20 -0700

    dropdown added to mobile nav bar

--- src/components/Shared/TopNav.js
+++ src/components/Shared/TopNav.js
@@ -1,19 +1,46 @@
 import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { widthContext } from "../../App";
 
 export default function TopNav() {
+  const width = useContext(widthContext);
+  //global variables in App.js
+
+  const mobile = window.$mobileWidth;
+
+  const [dropDown, setDropDown] = useState(false);
   return (
     <div className="TopNav--container">
       <img src="/mainAssets/assets/shared/desktop/logo.svg" alt="Log" />
-      <div className="Nav--links">
-        <Link to="Home">Home</Link>
-        <Link to="About">About Us</Link>
-        <Link to="Subscribe">Create Your Plan</Link>
-      </div>
-      <img
-        className="TopNav--hamburger"
-        src="\mainAssets\assets\shared\mobile\icon-hamburger.svg"
-        alt="hamburger icon"
-      />
+
+      {/* conditionally rendered if screen is larger than mobile or 
+     the state of dropdown is true(changed by clicking the hamburger menu) */}
+
+      {dropDown === true && width <= mobile && (
+        <img
+          onClick={() => setDropDown(!dropDown)}
+          className="TopNav--hamburger"
+          src="\mainAssets\assets\shared\mobile\icon-close.svg"
+          alt="X icon"
+        />
+      )}
+
+      {dropDown === false && (
+        <img
+          onClick={() => setDropDown(!dropDown)}
+          className="TopNav--hamburger"
+          src="\mainAssets\assets\shared\mobile\icon-hamburger.svg"
+          alt="hamburger icon"
+        />
+      )}
+
+      {(width > mobile || dropDown === true) && (
+        <div className="Nav--links">
+          <Link to="Home">Home</Link>
+          <Link to="About">About Us</Link>
+          <Link to="Subscribe">Create Your Plan</Link>
+        </div>
+      )}
     </div>
   );
 }
