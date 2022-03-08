efd348422886bd37a1c926d411657d1cb5ea3380
DKapteyn <86522409+DKapteyn@users.noreply.github.com>
2022-03-08 08:15:01 -0700

    added dynamic size change to bottom nav bar

--- src/components/Shared/BottomNav.js
+++ src/components/Shared/BottomNav.js
@@ -5,16 +5,16 @@ import { ReactComponent as Icon } from "./modifiedLogo.svg";
 export default function BottomNav() {
   return (
     <div className="BottomNav--container">
-      <div className="BottomNav--text">
-        <Icon fill="white" />
+      {/* <div className="BottomNav--text"> */}
+      <Icon fill="white" />
 
-        {/* SVG ADDED AS REACT COMPONENT SO NOT TO POLLUTE THE FILE. FILE WAS ALSO COPIED TO SRC SO THAT IT COULD BE IMPORTED AS A REACT FUNCTION  */}
-        <div className="Nav--linksBottom">
-          <Link to="Home">Home</Link>
-          <Link to="About">About Us</Link>
-          <Link to="Subscribe">Create Your Plan</Link>
-        </div>
+      {/* SVG ADDED AS REACT COMPONENT SO NOT TO POLLUTE THE FILE. FILE WAS ALSO COPIED TO SRC SO THAT IT COULD BE IMPORTED AS A REACT FUNCTION  */}
+      <div className="Nav--linksBottom">
+        <Link to="Home">Home</Link>
+        <Link to="About">About Us</Link>
+        <Link to="Subscribe">Create Your Plan</Link>
       </div>
+      {/* </div> */}
       <div className="BottomNav--socialMedia">
         <img
           src="/mainAssets/assets/shared/desktop/icon-facebook.svg"
