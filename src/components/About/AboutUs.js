e368240899102ebd1526b488884a2352118240ad
DKapteyn <86522409+DKapteyn@users.noreply.github.com>
2022-03-08 12:34:48 -0700

    dynamic styles added to AboutUS--HeroPic

--- src/components/About/AboutUs.js
+++ src/components/About/AboutUs.js
@@ -1,11 +1,37 @@
+import { widthContext } from "../../App";
+import { useContext } from "react";
+
 export default function AboutUs() {
+  const width = useContext(widthContext);
+  //global variables in App.js
+  const tablet = window.$tabletWidth;
+  const mobile = window.$mobileWidth;
+
   return (
     <div className="AboutUs--Container">
-      <img
-        src="/mainAssets/assets/about/desktop/image-hero-whitecup.jpg"
-        alt="Coffee Cup Pic"
-        className="AboutUs--heroPic"
-      />
+      {/* Image changes based on screen size   */}
+      {width > tablet ? (
+        //image for desktop
+        <img
+          src="/mainAssets/assets/about/desktop/image-hero-whitecup.jpg"
+          alt="Coffee Cup Pic"
+          className="AboutUs--heroPic"
+        />
+      ) : //image for tablet
+      width > mobile ? (
+        <img
+          src="/mainAssets/assets/about/tablet/image-hero-whitecup.jpg"
+          alt="Coffee Cup Pic"
+          className="AboutUs--heroPic"
+        />
+      ) : (
+        //image for mobile
+        <img
+          src="/mainAssets/assets/about/mobile/image-hero-whitecup.jpg"
+          alt="Coffee Cup Pic"
+          className="AboutUs--heroPic"
+        />
+      )}
       <div className="AboutUs--heroPicText">
         <div className="AboutUs--title">About Us</div>
         <div className="AboutUs--para">
